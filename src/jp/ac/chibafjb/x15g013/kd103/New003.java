cfdf1fc84dc519a4fbafe68cd5d0d3596dd6e45e
unknown <x15g013@PC504N028.chiba-fjb.ac.jp>
2015-11-02 14:48:18 +0900

    ３回目

---
+++ src/jp/ac/chibafjb/x15g013/kd103/New003.java
@@ -0,0 +1,10 @@
+package jp.ac.chibafjb.x15g013.kd103;
+
+public class New003 {
+
+	public static void main(String[] args) {
+		System.out.print("1 船橋太郎 2 船橋太郎 3 船橋太郎 4 船橋太郎 5 船橋太郎 6 船橋太郎 7 船橋太郎 8 船橋太郎 9 船橋太郎 10 船橋太郎");
+
+	}
+
+}
