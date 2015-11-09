53a480c3616e2f7d7dcd6d5772e4c5cf4403c436
unknown <x15g013@PC504N028.chiba-fjb.ac.jp>
2015-11-09 14:02:57 +0900

    114~116

---
+++ src/jp/ac/chibafjb/x15g013/kd115/New115.java
@@ -0,0 +1,14 @@
+package jp.ac.chibafjb.x15g013.kd115;
+
+public class New115 {
+
+	public static void main(String[] args) {
+		for(int i=1;i<=2;i++)
+			System.out.println("あいうえお　あいうえお　あいうえお");
+		for(int i=1;i<=2;i++)
+			System.out.println("かきくけこ　かきくけこ　かきくけこ");
+		for(int i=1;i<=2;i++)
+			System.out.println("さしすせそ　さしすせそ　さしすせそ");
+	}
+
+}
---
+++ src/jp/ac/chibafjb/x15g013/kd116/New116.java
@@ -0,0 +1,12 @@
+package jp.ac.chibafjb.x15g013.kd116;
+
+public class New116 {
+
+	public static void main(String[] args) {
+		for(int i=1;i<=2;i++)
+			System.out.println("あいうえお　あいうえお　あいうえお\n"
+					+ "かきくけこ　かきくけこ　かきくけこ\n"
+					+ "さしすせそ　さしすせそ　さしすせそ");			
+	}
+
+}
