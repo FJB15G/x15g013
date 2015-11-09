08a3e016b31b440f6fcae3cd1d54d8c468d63138
unknown <x15g013@PC504N028.chiba-fjb.ac.jp>
2015-11-09 14:43:02 +0900

    137~138

---
+++ src/jp/ac/chibafjb/x15g013/kd137/New137.java
@@ -0,0 +1,17 @@
+package jp.ac.chibafjb.x15g013.kd137;
+
+public class New137 {
+
+	public static void main(String[] args) {
+		int a = 100;
+		int b = 1;
+		int c = a;
+		a = b;
+		b = c;
+		System.out.println("a = "+a);
+		System.out.println("b = "+b);
+		System.out.println("c = "+c);
+
+	}
+
+}
---
+++ src/jp/ac/chibafjb/x15g013/kd138/New138.java
@@ -0,0 +1,19 @@
+package jp.ac.chibafjb.x15g013.kd138;
+
+public class New138 {
+
+	public static void main(String[] args) {
+		int a = 1;
+		int b = 2;
+		int c = 3;
+		int d = a;
+		a = b;
+		b = c;
+		c = d;
+		System.out.println("a = "+a);
+		System.out.println("b = "+b);
+		System.out.println("c = "+c);
+
+	}
+
+}
